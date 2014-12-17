0417b79aeb20ea54e0fff562aac904284128b17f
Mendelevium <martindionne@gmail.com>
2014-12-16 22:40:18 -0500

    added comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,16 +1,24 @@
 ## Cache and Return a matrix that is the inverse of 'x'
 
 ## Cache a matrix that is the inverse of 'x'
-
 makeCacheMatrix <- function(x = matrix()) {
     m <- NULL
+    
+    # set the value of the matrix
     set <- function(y) {
         x <<- y
         m <<- NULL
     }
+    # get the value of the matrix
     get <- function() x
+    
+    # set the value of the inverse
     set_inv <- function(not_inv) m <<- not_inv
+    
+    # get the value of the inverse
     get_inv <- function() m
+    
+    # create a list of index
     list(set = set, get = get,
          set_inv = set_inv,
          get_inv = get_inv)
@@ -18,29 +26,40 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 ## Return a matrix that is the inverse of 'x'
-
 cacheSolve <- function(x, ...) {
+    
+    # get the inverse from the cache
     m <- x$get_inv()
+    
+    # if it already exist, return it
     if(!is.null(m)) {
         message("getting cached data")
         return(m)
     }
+    
+    # else, get the matrix
     data <- x$get()
+   
+    # inverse it
     m <- solve(data, ...)
+    
+    # save it
     x$set_inv(m)
+    
+    # return it
     m
 }
 
 
 ## How to use?
-## library(Matrix)
-## u <- Matrix(rnorm(9),3)
-## a <- makeCacheMatrix(u)
-## v <- cacheSolve(a)
+# library(Matrix)
+# u <- Matrix(rnorm(9),3)
+# a <- makeCacheMatrix(u)
+# v <- cacheSolve(a)
 ## verify with
-## u %*% v
+# u %*% v
 
 ## optionnaly use 
-## a$get() or a$set(w)
+# a$get() # or a$set(w)
 ## to get or set the stored data
 
