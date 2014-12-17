15877071dd61be47ef4ea4193a235ddb32d79bbd
Mendelevium <martindionne@gmail.com>
2014-12-16 22:15:13 -0500

    initial commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,46 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Cache and Return a matrix that is the inverse of 'x'
 
-## Write a short comment describing this function
+## Cache a matrix that is the inverse of 'x'
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    m <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    set_inv <- function(not_inv) m <<- not_inv
+    get_inv <- function() m
+    list(set = set, get = get,
+         set_inv = set_inv,
+         get_inv = get_inv)
 }
 
 
-## Write a short comment describing this function
+## Return a matrix that is the inverse of 'x'
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    m <- x$get_inv()
+    if(!is.null(m)) {
+        message("getting cached data")
+        return(m)
+    }
+    data <- x$get()
+    m <- solve(data, ...)
+    x$set_inv(m)
+    m
 }
+
+
+## How to use?
+## library(Matrix)
+## u <- Matrix(rnorm(9),3)
+## a <- makeCacheMatrix(u)
+## v <- cacheSolve(a)
+## verify with
+## u %*% v
+
+## optionnaly use 
+## a$get() or a$set(w)
+## to get or set the stored data
+
